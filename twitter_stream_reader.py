ce8a322885929fc91d534b3113f9f36ad50b52bb
Andrew Young <andrew@vaelen.org>
2016-08-16 13:10:10 -0500

    Added a progress bar. Cleaned up the code some more. Added the ability to ask for a certain number of tweets.

--- twitter_stream_reader.py
+++ twitter_stream_reader.py
@@ -22,6 +22,7 @@ import sys
 import json
 import argparse
 import dateutil.parser
+import progressbar
 
 from pymongo import MongoClient
 from configparser import RawConfigParser
@@ -40,21 +41,24 @@ class TweetHelper:
         self.raw = self.db.raw
 
     def copy_from_raw(self):
-        self.log("Dropping Tweet Collection")
+        log("Dropping Tweet Collection")
         self.tweets.drop()
-        self.log("Creating Tweet Collection From Raw Collection")
+        log("Creating Tweet Collection From Raw Collection")
+        total = self.raw.count()
         cursor = self.raw.find({})
         i = 0
-        for tweet in cursor:
+        with progressbar.ProgressBar(max_value=total) as bar:
+          for tweet in cursor:
             self.insert_tweet(tweet)
             i = i + 1
-            if i % 100 == 0: sys.stderr.write("#")
+            # if i % 100 == 0: sys.stderr.write("#")
+            bar.update(i)
 
     def generate_doc(self, tweet):
         doc = {}
-        doc["id"] = tweet["id"]
+        doc["id"] = tweet["id_str"]
         doc["user"] = {
-            "id": tweet["user"]["id"],
+            "id": tweet["user"]["id_str"],
             "name": tweet["user"]["name"],
             "screen_name": tweet["user"]["screen_name"],
             "description": tweet["user"]["description"],
@@ -62,6 +66,7 @@ class TweetHelper:
             "location": tweet["user"]["location"],
             "created_at": dateutil.parser.parse(tweet["user"]["created_at"])
         }
+        doc["coordinates"] = tweet["coordinates"]
         doc["place"] = tweet["place"]
         doc["text"] = tweet["text"]
         doc["lang"] = tweet["lang"]
@@ -81,33 +86,53 @@ class TweetHelper:
 
     def parse_json(self, data):
         tweet = json.loads(data)
-        if not "user" in tweet: return True
+        if not "user" in tweet: return False
         sys.stderr.write(".")
         self.raw.insert(tweet)
         self.insert_tweet(tweet)
-  
-# This is a listener that stores tweets in MongoDB
-class IngestListener(StreamListener):
-  def __init__(self):
-    self.tweet_helper = TweetHelper()
+        return True
 
+# This is an abstract listener that implements
+class BaseListener(StreamListener):
+  def __init__(self, total):
+    self.i = 0
+    self.total = total
+      
+# This is a listener that stores tweets in MongoDB
+class IngestListener(BaseListener):
+  def __init__(self, total):
+    super().__init__(total)
+    self.tweet_helper = TweetHelper()
+    if total < 1:
+      self.bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength,redirect_stderr=True)
+    else:
+      self.bar = progressbar.ProgressBar(max_value=total,redirect_stderr=True)
+      
   def on_data(self, data):
-    self.tweet_helper.parse_json(data)
+    if self.tweet_helper.parse_json(data):
+      self.i = self.i + 1
+      self.bar.update(self.i)
+    if self.total > 0 and self.i >= self.total: return False
     return True
 
   def on_error(self, status):
     log("Error: ", status)
 
 # This is a listener that just prints received tweets to stdout.
-class WatchListener(StreamListener):
+class WatchListener(BaseListener):
+  def __init__(self, total):
+    super().__init__(total)
+
   def on_data(self, data):
     tweet = json.loads(data)
     if not "user" in tweet: return True
     if not "text" in tweet: return True
+    self.i = self.i + 1
     print("User: ", tweet["user"]["name"])
     print("Description: ", tweet["user"]["description"])
     print("Tweet: ", tweet["text"])
     print("------------------------------------------------------------")
+    if self.total > 0 and self.i >= self.total: return False
     return True
 
   def on_error(self, status):
@@ -118,6 +143,7 @@ def parse_arguments():
                                    epilog='By default a random sampling of tweets will be collected and stored in two collections: "raw" and "tweets".  '
                                    + ' The "raw" collection contains the raw tweet data.  The "tweets" collection contains a subset of this data.')
   parser.add_argument('-t', '--topics', metavar='TOPIC', nargs='*', help='Track the given topics. This disables random sampling.')
+  parser.add_argument('-m', '--max', metavar='COUNT', default=0, type=int, help='Stop after COUNT tweets.')
   parser.add_argument('-w', '--watch', action='store_true', help='Watch stream. This will print tweets to standard out instead of storing them in MongoDB.')
   parser.add_argument('-c', '--convert', action='store_true', help='Convert tweets. This will re-process all tweets in the raw collection instead of reading new tweets.')
   parser.add_argument('-f', '--config', metavar='FILE', default='twitter.cfg', help='Config file. Default: twitter.cfg')
@@ -147,10 +173,10 @@ def main():
     # Determine which listener to use
     listener = None
     if options.watch:
-      listener = WatchListener()
+      listener = WatchListener(options.max)
       log("Watching instead of storing tweets.")
     else:
-      listener = IngestListener()
+      listener = IngestListener(options.max)
     
     # This handles Twitter authetification and the connection to Twitter Streaming API
     auth = OAuthHandler(consumer_key, consumer_secret)
